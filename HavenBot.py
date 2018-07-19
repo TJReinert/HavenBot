fd64fe56a4b104069aa7592c0a9453cef28f0616
Jinkoo <swimy2012@gmail.com>
2018-07-18 23:16:16 -0500

    Refactor out permission checks, add methods to set username and avatar.

--- HavenBot.py
+++ HavenBot.py
@@ -1,9 +1,14 @@
-import discord, sys
-from discord.ext import commands
-from Welcome import Welcome
 from pathlib import Path
 
+import discord
+import sys
+from aiohttp import ClientSession
+from discord.ext import commands
+
+from Welcome import Welcome
+
 bot = commands.Bot(command_prefix='$', description='A bot that greets the user back.')
+client = discord.Client().user
 
 
 @bot.event
@@ -14,6 +19,9 @@ async def on_ready():
     print('token: ' + get_token())
     print('------')
 
+
+async def is_admin(ctx):
+    return ctx.message.author.guild_permissions.administrator
 
 # WelcomeBot Accessors
 welcome = Welcome()
@@ -35,37 +43,66 @@ async def welcome_member(member):
     await channel.send(message.format(member))
 
 
+@commands.check(is_admin)
 @bot.command()
 async def toggleWelcome(ctx):
-    if ctx.message.author.guild_permissions.administrator:
-        new_state = 'enabled' if welcome.toggle() else 'disabled'
-        await ctx.send("Welcome messeges have been {}.".format(new_state))
+    new_state = 'enabled' if welcome.toggle() else 'disabled'
+    await ctx.send("Welcome messeges have been {}.".format(new_state))
 
 
+@commands.check(is_admin)
 @bot.command()
 async def setWelcome(ctx, *, arg):
-    if ctx.message.author.guild_permissions.administrator:
-        welcome.set_welcome_message(arg)
-        await ctx.send("Welcome messeges have been set to {}.".format(arg)
-                       + '\n'
-                       + "It will look like this when someone joins."
-                       + '\n')
-        await welcome_member(ctx.author)
+    welcome.set_welcome_message(arg)
+    await ctx.send("Welcome messeges have been set to {}.".format(arg)
+                   + '\n'
+                   + "It will look like this when someone joins."
+                   + '\n')
+    await welcome_member(ctx.author)
 
 
+@commands.check(is_admin)
 @bot.command()
 async def welcomeHere(ctx):
-    if ctx.message.author.guild_permissions.administrator:
-        welcome.set_channel_id(str(ctx.channel.id))
-        await ctx.send("Okay, I will welcome new members in {0.channel.mention}".format(ctx))
+    welcome.set_channel_id(str(ctx.channel.id))
+    await ctx.send("Okay, I will welcome new members in {0.channel.mention}".format(ctx))
 
 
+@commands.check(is_admin)
 @bot.command()
 async def shutdown(ctx):
-    if ctx.message.author.guild_permissions.administrator:
-        await ctx.send("ByeBye!")
-        # todo get context for user who executed
-        sys.exit("{0.message.author.name} executed shutdown command".format(ctx))
+    await ctx.send("ByeBye!")
+    sys.exit("{0.message.author.name} executed shutdown command".format(ctx))
+
+
+@commands.check(is_admin)
+@bot.command()
+async def setAvatar(ctx, arg=None):
+    image_url = await determine_image_url(arg, ctx)
+    if image_url is None:
+        ctx.send("Did you pass in a URL or Image?")
+    async with ClientSession() as session:
+        async with session.get(image_url) as response:
+            image = await response.read()
+            await ctx.bot.user.edit(avatar=image)
+            await ctx.send("I am an enigma. I have become {}".format(image_url))
+    # await ctx.send("An error occurred. Status code {}".format(r.status))
+
+
+async def determine_image_url(image_url, ctx):
+    if image_url is None:
+        image_url = ctx.message.attachments[0].url
+    return image_url
+
+
+@commands.check(is_admin)
+@bot.command()
+async def setUserName(ctx, *, arg):
+    if len(arg) < 2:
+        await ctx.send("Name must be longer than 2 characters")
+    else:
+        await ctx.bot.user.edit(username=arg[:32])
+        await ctx.send("All hail {}!".format(arg))
 
 
 def get_token():
