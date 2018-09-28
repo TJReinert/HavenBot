c4196310d5aef48f8bf2d5461234aad1bb66c0dd
Jinkoo <swimy2012@gmail.com>
2018-09-28 18:12:39 -0500

    Extract into own cog, add support for more cogs, make configuration helper accept multiple sections

--- ConfigurationHelper.py
+++ ConfigurationHelper.py
@@ -10,6 +10,7 @@ class ConfigUtil:
         self.section_name = section_name
 
     def config_exists(self):
+        self.read()
         path = Path(self.settings_file)
         if path.exists():
             self.config.read(self.settings_file)
@@ -18,11 +19,13 @@ class ConfigUtil:
             return False
 
     def write_new_settings(self, settings):
+        self.read()
+
         section = self.section_name
         config = self.config
 
         config.add_section(section)
-        for option, value in settings:
+        for option, value in settings.items():
             config.set(section, option, value)
 
         self.save()
@@ -31,10 +34,41 @@ class ConfigUtil:
         with open(self.settings_file, 'w') as configfile:
             self.config.write(configfile)
 
-    def get(self, option):
+    def read(self):
         self.config.read(self.settings_file)
+
+    def get(self, option):
+        self.read()
         return self.config.get(self.section_name, option)
 
     def set(self, option, value):
+        self.read()
         self.config.set(self.section_name, option, value)
         self.save()
+
+    def update_defaults(self, defaults):
+        """Sets the defaults for the module if the key does not exist,
+        if there are extras that have been deprecated they will be removed"""
+        self.read()
+        for key in self.config[self.section_name]:
+            if key not in defaults.keys():
+                self.config.remove_option(self.section_name, key)
+
+        for key, value in defaults.items():
+            if key not in self.config[self.section_name]:
+                self.config.set(self.section_name, key, value)
+
+        self.save()
+
+
+if __name__ == '__main__':
+    configUtil = ConfigUtil('welcome')
+    var = {
+        'enabled': 'True',
+        'message': '{0.mention}, welcome to {0.guild.name}!',
+        'welcome_channel': '-1',
+        'leave_channel': '-1',
+        'leave_message': '{0.display_name}#{0.discriminator} has left the server. :wave:',
+        'newval': 'wooooyhooo'
+    }
+    configUtil.update_defaults(var)
